7eaf34aefb6654ea65fbb5a01af93de4e9fb7170
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-15 22:01:25 +0000

    logic for photos

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import { setConstantValue } from "typescript";
 
-const imgs = [
+const IMGS = [
   {
     name: "img1",
     src: "/gallery/no_doseqs.jpg",
     alt: "bike",
     width: 200,
     height: 1,
-    aspect: "aspect-",
-    span: "sm:row-span-2 sm:row-end-auto",
+    aspect: "md:aspect-auto",
+    span: "md:row-span-2 md:row-end-auto",
     caption: "the most interesting man",
   },
   {
@@ -18,8 +19,8 @@ const imgs = [
     alt: "",
     width: 200,
     height: 1,
-    aspect: "aspect-[2/1]",
-    span: "sm:col-span-2 sm:col-end-auto",
+    aspect: "md:aspect-[2/1]",
+    span: "md:col-span-2 md:col-end-auto",
     caption: "the birds are taking a bath",
   },
   {
@@ -54,38 +55,67 @@ const imgs = [
 // const imgs = require.context("../public/gallery", true, /\.(png|jpe?g|svg)$/);
 
 const photos = () => {
+  const [imgs, setImg] = useState(IMGS);
+  // const [flip, setFlip] = useState([]);
+  // map image names to flip states
+  const [flip, setFlip] = useState(imgs.map((img) => false));
+
   // preprocess the images
-  // imgs.map((img) => {
-  //   // if aspect ratio is 2/3, then set span to 2 rows
-  //   if (img.aspect === "aspect-[2/3]") {
-  //     img.span = "row-start-2 row-end-1";
-  //   }
-  // });
   return (
     <>
-      This is the photos page. Useless stuff.
-      <div className="container mx-auto">
+      random photos
+      <div className="relative container mx-auto z-0">
         {/* <div className="grid gap-3 grid-cols-3 max-sm:grid-cols-1 max-md:grid-cols-2"> */}
-        <div className="grid gap-2 grid-cols-16 ">
-          {imgs.map((img) => (
+        <div className="grid gap-2 grid-cols-16 grid-rows-16">
+          {imgs.map((img, index) => (
             <button
               key={img.name}
-              // if the aspect ration is 2/3, then set span to 2 rows
-
-              className={`group flex relative ${img.span} ${img.aspect}  rounded-xl 
-              hover:shadow-2xl hover:scale-105 hover:backdrop-blur-xl hover:z-40
+              className={`group relative aspect-square ${img.span} ${
+                img.aspect
+              } rounded-xl
+              ${flip[index] ? "shadow-2xl scale-105 z-10" : ""}
+              sm:hover:shadow-2xl sm:hover:scale-105 sm:hover:z-10
               ease-in-out duration-500 focus:outline-2 outline-black`}
+              onClick={() => {
+                // flip.set(img, !flip.get(img));
+                // if screen size is small, flip the image
+
+                if (window.innerWidth < 640) {
+                  const old = flip[index];
+                  const newd = flip.slice(index);
+                  // set all of newd to false
+                  for (let i = 0; i < newd.length; i++) {
+                    newd[i] = false;
+                  }
+                  newd[index] = !old;
+                  setFlip(newd);
+                } else {
+                  const newd = flip.slice(index);
+                  // set all of newd to false
+                  for (let i = 0; i < newd.length; i++) {
+                    newd[i] = false;
+                  }
+                  setFlip(newd);
+                }
+                // console.log(flip[index]);
+              }}
             >
               <Image
                 src={img.src}
                 alt={img.alt}
-                // width="400"
-                // height={img.height}
                 fill
                 sizes=""
-                className="relative object-cover overflow-hidden rounded-xl shadow-xl group-hover:brightness-50"
+                className={`z-0 flex object-cover rounded-xl shadow-xl sm:group-hover:brightness-50 ${
+                  flip[index] ? "max-sm:brightness-50" : "max-sm:brightness-100"
+                }`}
               />
-              <p className="flex absolute inset-0 p-10 z-50 opacity-0 justify-center items-center group-hover:opacity-100 group-hover:brightness-100 duration-500 text-white ">
+              <p
+                className={`flex absolute inset-0 p-10 z-10
+                opacity-0 justify-center items-center 
+                sm:group-hover:opacity-100
+                ${flip[index] ? "opacity-100" : ""}
+                duration-500 ease-in-out text-white`}
+              >
                 {img.caption ? img.caption : "caption"}
               </p>
             </button>
