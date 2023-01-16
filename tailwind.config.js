72667d4a5376aae811f1d31dfd740216b5b06c1d
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-16 06:41:22 +0000

    add custom color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      dark_blue: "#22223B",
+      blue: "#4A4E69",
+      light_blue: "#9A8C98",
+      pink: "#C9ADA7",
+      white: "#F2E9E4",
+    },
+
     extend: {
       gridTemplateColumns: {
         16: "repeat(auto-fit, minmax(240px, 1fr))",
