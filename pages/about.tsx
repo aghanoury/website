baad980a6dc449c3e85590663099df1320bc889c
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-16 06:41:48 +0000

    minor update

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <>
-            <h1 className="text-xl">About</h1>
-            <p>Some sample text...</p>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sunt doloribus quia consectetur exercitationem facilis eveniet. Laboriosam officiis odio laudantium ullam, repudiandae alias vitae maiores aliquam reiciendis illum neque. Placeat!</p>
-        </>
-    );
+  return (
+    <>
+      <h1 className="text-xl">About</h1>
+      <p>
+        This is the about me page. I was born and raised in Colorado. I went to
+        school.
+      </p>
+    </>
+  );
 };
 
 export default About;
