52b100f5dd556c46a896acf6c551e3b8128483cd
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-16 06:43:15 +0000

    new images, new logic

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -6,49 +6,129 @@ const IMGS = [
   {
     name: "img1",
     src: "/gallery/no_doseqs.jpg",
-    alt: "bike",
-    width: 200,
-    height: 1,
     aspect: "md:aspect-auto",
     span: "md:row-span-2 md:row-end-auto",
-    caption: "the most interesting man",
+    alt: "the most interesting man, some bar in new orleans",
   },
   {
     name: "img2",
     src: "/gallery/bird-bath.jpg",
-    alt: "",
-    width: 200,
-    height: 1,
     aspect: "md:aspect-[2/1]",
     span: "md:col-span-2 md:col-end-auto",
-    caption: "the birds are taking a bath",
+    alt: "the birds took a bath",
   },
   {
     name: "img3",
     src: "/gallery/random-mirror.jpg",
-    alt: "",
-    width: 200,
-    height: 1,
     aspect: "aspect-square",
-    caption: "found some mirrors in sf",
+    alt: "found some mirrors in sf",
+  },
+  {
+    name: "lacy girl",
+    src: "/gallery/lacybnw.jpg",
+    aspect: "md:aspect-square",
+    alt: "lacy girl after dinner",
   },
   {
     name: "img4",
     src: "/gallery/mountain.jpg",
-    alt: "",
-    width: 200,
-    height: 1,
     aspect: "aspect-square",
-    caption: "mountain valley, co",
+    alt: "mountain valley, co",
   },
   {
     name: "img5",
     src: "/gallery/cam_peak.JPG",
-    alt: "",
-    width: 200,
-    height: 1,
     aspect: "aspect-square",
-    caption: "cam peak, co",
+    alt: "cam peak, co",
+  },
+  // {
+  //   name: "img6",
+  //   src: "/gallery/drama.jpg",
+  //   aspect: "md:aspect-[2/1]",
+  //   span: "md:col-span-2 md:col-end-auto",
+  //   alt: "missed the shot",
+  // },
+  {
+    name: "fire",
+    src: "/gallery/fire.jpg",
+    aspect: "md:aspect-square",
+    alt: "calwood fire, october 2020",
+  },
+  {
+    name: "greyrock",
+    src: "/gallery/greyrock.jpg",
+    aspect: "md:aspect-square",
+    alt: "greyrock mountain, co",
+  },
+  {
+    name: "greyrock_view",
+    src: "/gallery/greyrock_view.jpg",
+    aspect: "md:aspect-[2/1]",
+    span: "md:col-span-2 md:col-end-auto",
+    alt: "view from greyrock",
+  },
+  {
+    name: "moose",
+    src: "/gallery/moose.jpg",
+    aspect: "md:aspect-square",
+    alt: "moose in the water",
+  },
+  {
+    name: "meadowc1",
+    src: "/gallery/meadow_creek1.jpg",
+    aspect: "md:aspect-square",
+    alt: "meadow creek, co",
+  },
+  {
+    name: "blizzard",
+    src: "/gallery/glenwood_springs_blizzard.jpg",
+    aspect: "md:aspect-square",
+    alt: "blizzard in glenwood springs",
+  },
+  {
+    name: "meadowc2",
+    src: "/gallery/meadow_creek2.jpg",
+    aspect: "md:aspect-[3/1]",
+    span: "md:col-span-3 md:col-end-auto",
+    alt: "the ducks of meadow creek",
+  },
+  {
+    name: "chief mountain",
+    src: "/gallery/chief_mountain.jpg",
+    aspect: "md:aspect-auto",
+    span: "md:row-span-2 md:row-end-auto",
+    alt: "view from cheif mountain, co",
+  },
+  {
+    name: "some mountain",
+    src: "/gallery/some_mountain.jpg",
+    aspect: "md:aspect-square",
+    alt: "some snowy mountain",
+  },
+  {
+    name: "taco classic closeup",
+    src: "/gallery/taco_closeup.jpg",
+    aspect: "md:aspect-square",
+    alt: "taco 💕",
+  },
+  {
+    name: "port of sf",
+    src: "/gallery/port_of_sf.jpg",
+    aspect: "md:aspect-[2/1]",
+    span: "md:col-span-2 md:col-end-auto",
+    alt: "port of sf",
+  },
+  {
+    name: "airplane window",
+    src: "/gallery/airplane_window1.jpg",
+    aspect: "md:aspect-square",
+    alt: "east bay from the sky",
+  },
+  {
+    name: "taco chain",
+    src: "/gallery/taco_chain.jpg",
+    aspect: "md:aspect-square",
+    alt: "taco's gold chain",
   },
 ];
 
@@ -56,67 +136,71 @@ const IMGS = [
 
 const photos = () => {
   const [imgs, setImg] = useState(IMGS);
-  // const [flip, setFlip] = useState([]);
-  // map image names to flip states
-  const [flip, setFlip] = useState(imgs.map((img) => false));
+  // const [selected, setselected] = useState([]);
+  // map image names to selected states
+  const [selected, setselected] = useState(imgs.map((img) => false));
 
   // preprocess the images
   return (
     <>
-      random photos
+      <div className="flex justify-center m-5">
+        Random photos from 2021 and 2022
+      </div>
       <div className="relative container mx-auto z-0">
         {/* <div className="grid gap-3 grid-cols-3 max-sm:grid-cols-1 max-md:grid-cols-2"> */}
         <div className="grid gap-2 grid-cols-16 grid-rows-16">
           {imgs.map((img, index) => (
             <button
               key={img.name}
-              className={`group relative aspect-square ${img.span} ${
-                img.aspect
-              } rounded-xl
-              ${flip[index] ? "shadow-2xl scale-105 z-10" : ""}
+              className={`group relative aspect-square ${img.span} ${img.aspect}
+              ${selected[index] ? "shadow-2xl scale-105 z-10" : ""}
               sm:hover:shadow-2xl sm:hover:scale-105 sm:hover:z-10
               ease-in-out duration-500 focus:outline-2 outline-black`}
               onClick={() => {
-                // flip.set(img, !flip.get(img));
-                // if screen size is small, flip the image
+                // selected.set(img, !selected.get(img));
+                // if screen size is small, selected the image
 
                 if (window.innerWidth < 640) {
-                  const old = flip[index];
-                  const newd = flip.slice(index);
+                  const old = selected[index];
+                  const newd = selected.slice(index);
                   // set all of newd to false
                   for (let i = 0; i < newd.length; i++) {
                     newd[i] = false;
                   }
                   newd[index] = !old;
-                  setFlip(newd);
+                  setselected(newd);
                 } else {
-                  const newd = flip.slice(index);
+                  const newd = selected.slice(index);
                   // set all of newd to false
                   for (let i = 0; i < newd.length; i++) {
                     newd[i] = false;
                   }
-                  setFlip(newd);
+                  setselected(newd);
                 }
-                // console.log(flip[index]);
+                // console.log(selected[index]);
               }}
             >
               <Image
                 src={img.src}
                 alt={img.alt}
                 fill
-                sizes=""
-                className={`z-0 flex object-cover rounded-xl shadow-xl sm:group-hover:brightness-50 ${
-                  flip[index] ? "max-sm:brightness-50" : "max-sm:brightness-100"
+                sizes="(max-width: 768px) 100vw,
+                (max-width: 1200px) 50vw,
+                33vw"
+                className={`z-0 flex object-cover rounded-lg shadow-xl sm:group-hover:brightness-50 ${
+                  selected[index]
+                    ? "max-sm:brightness-50"
+                    : "max-sm:brightness-100"
                 }`}
               />
               <p
                 className={`flex absolute inset-0 p-10 z-10
                 opacity-0 justify-center items-center 
                 sm:group-hover:opacity-100
-                ${flip[index] ? "opacity-100" : ""}
+                ${selected[index] ? "opacity-100" : ""}
                 duration-500 ease-in-out text-white`}
               >
-                {img.caption ? img.caption : "caption"}
+                {img.alt ? img.alt : "some sick ass photo"}
               </p>
             </button>
           ))}
