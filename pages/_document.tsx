12855f91e9f5761c47b95644190a3d1e7bdaa71a
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-16 06:41:39 +0000

    global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
+// top level styles
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-white text-dark_blue">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
