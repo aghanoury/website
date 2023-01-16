7215c8b8af6253850534b9a26988f94e9e3beedf
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-16 06:42:14 +0000

    add profile image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,35 @@
 import Head from "next/head";
+import React from "react";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import Navbar from "./navbar";
+import { Transition } from "@headlessui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // set a variable true after 1 second
+
   return (
     <>
+      <h1 className="text-xl"></h1>
       <div className="text-justify">
-        <h1 className="text-xl"></h1>
-        <p className="my-2">
+        <div
+          className={`relative container w-60 mx-auto sm:mx-5 my-5 
+        aspect-square max-w-sm sm:float-right`}
+        >
+          <Image
+            src="/gallery/profile.jpg"
+            alt="Picture of the author"
+            fill
+            sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            33vw"
+            className="rounded-full absolute transition"
+          />
+        </div>
+        <p className="">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
           unde tempora soluta quidem repellendus adipisci rerum, sapiente itaque
           fugiat voluptate officiis quisquam quos nam tenetur accusamus dolorem
