01c30d88505db9fcb7f24f4286a4a5db7a9f49a5
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-22 09:18:22 +0000

    fixed content

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,7 +4,7 @@ const Blog = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-xl m-5">Blog</h1>
-      <p className="text-justify sm:p-10">nothing to blog about yet...</p>
+      <p className="text-justify sm:p-10">Nothing to blog about yet...</p>
     </div>
   );
 };
--- pages/research.tsx
+++ pages/research.tsx
@@ -4,7 +4,7 @@ const Research = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-xl m-5">Research</h1>
-      <p className="text-justify sm:p-10">404 not found</p>
+      <p className="text-justify sm:p-10">Nothing to post yet...</p>
     </div>
   );
 };
