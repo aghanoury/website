f8041c8d96a8866342f4d28ffb8e9c58c332830f
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-22 08:30:54 +0000

    page update

--- pages/research.tsx
+++ pages/research.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Research = () => {
   return (
-    <div>
-      <h1 className="text-xl">Research</h1>
-      <div>404 not found</div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl m-5">Research</h1>
+      <p className="text-justify sm:p-10">404 not found</p>
     </div>
   );
 };
