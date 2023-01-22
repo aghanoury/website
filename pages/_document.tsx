fb24cc5addfbd466f67d54d6eefa4d94cbee980b
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-01-22 08:30:02 +0000

    add fav icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/favicon.png" />
+      <link rel="mask-icon" href="/favicon.png" />
       <body className="bg-white text-dark_blue dark:bg-dark_blue dark:text-white">
         <Main />
         <NextScript />
