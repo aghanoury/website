c50532539154e35239696df5fd50a18bc2c9088b
Pooya Aghanoury <48891215+aghanoury@users.noreply.github.com>
2023-02-06 11:19:48 -0800

    typo fix

--- pages/research.tsx
+++ pages/research.tsx
@@ -7,8 +7,8 @@ const Research = () => {
       <h1 className="text-xl m-5">Research</h1>
       {/* <p className="text-justify sm:p-10">Nothing to post yet...</p> */}
       <Hpar>
-        My current research is aimed at finding new ways to chiplet based
-        systems more secure and efficient{" "}
+        My current research is aimed at finding new ways to make chiplet based
+        systems more secure and efficient.{" "}
       </Hpar>
     </div>
   );
