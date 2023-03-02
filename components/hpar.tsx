f7f8dd03044489736cb7cf8fbd7289d4db80ed0b
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-03-02 07:30:11 +0000

    removed box around text

--- components/hpar.tsx
+++ components/hpar.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hpar = (props) => {
   return (
     <div
-      className={`rounded-lg p-4 bg-pink dark:bg-blue flex flex-col space-y-2`}
+      // className={`rounded-lg p-4 bg-pink dark:bg-blue flex flex-col space-y-2`}
+      className={`rounded-lg p-4 flex flex-col space-y-2 break-words`}
     >
       {props.children}
     </div>
